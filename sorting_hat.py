bae622e0dc8cfbc71dcc95af3b4fa96c4637a5e5
Shinya Hayashi <shinyaUT0925@gmail.com>
2023-08-19 11:26:50 +0900

    load various configurations from input file in the YAML format.
    Signed-off-by: Shinya Hayashi <shinyaUT0925@gmail.com>

--- sorting_hat.py
+++ sorting_hat.py
@@ -4,9 +4,17 @@ from simanneal import Annealer
 import random
 import copy
 import sys
+import yaml
 
 INFINITY = 10000
 INVALID_CLASS_ID = -1
+
+class Settings:
+    num_teams = 0
+    num_members_in_each_team = []
+    members = []
+
+settings = Settings()
 
 class Preference:
     class_affinity = INVALID_CLASS_ID
@@ -42,6 +50,8 @@ class SortingHat(Annealer):
         v = 0.0
         for ti, team in enumerate(self.state):
             for member in team:
+                if member not in self.preferences:
+                    continue
                 if self.preferences[member].class_affinity not in [INVALID_CLASS_ID, ti]:
                     v += INFINITY
                 if len(team) < self.preferences[member].num_min_team_members:
@@ -49,6 +59,8 @@ class SortingHat(Annealer):
 
         for hi, hist in enumerate(self.history):
             for ti, team in enumerate(self.state):
+                if len(hist) <= ti:
+                    continue
                 for member in team:
                     if member in hist[ti]:
                         v += (hi+2.0)/(hi+1.0)
@@ -64,41 +76,83 @@ class SortingHat(Annealer):
         for team in teams:
             if member in team:
                 return team
-        print("team not found for {}.".format(team))
-        sys.exit(1)
+        return set()
 
     def get_other_members(self, member, team):
+        if member not in team:
+            return set()
         tmpTeam = copy.copy(team)
         tmpTeam.remove(member)
         return tmpTeam
 
+
+def load_input(file_name):
+    with open(file_name, 'r') as yml:
+        config = yaml.safe_load(yml)
+
+    load_settings(config['settings'])
+    h = load_history(config['history'])
+    p = load_preferences(config['preferences'])
+    return h, p
+
+def load_settings(input_settings):
+    settings.num_teams = input_settings['num_teams']
+    settings.num_members_in_each_team = input_settings['num_members_in_each_team']
+    settings.members = input_settings['members']
+
+    if sum(settings.num_members_in_each_team) != len(settings.members):
+        print("invalid settings.")
+        sys.exit(1)
+
+def load_history(input_history):
+    history = []
+    for hist in input_history:
+        tmpHist = []
+        tmpTeam = set()
+        for team in hist:
+            for member in team:
+                tmpTeam.add(member)
+            tmpHist.append(copy.copy(tmpTeam))
+            tmpTeam.clear()
+        history.append(copy.copy(tmpHist))
+        tmpHist.clear()
+
+    return history
+
+def load_preferences(input_preferences):
+    preferences = dict()
+    for pref in input_preferences:
+        name = pref['name']
+        if name not in settings.members:
+            print("preference for invalid member '{}' found.".format(name))
+            sys.exit(1)
+        class_affinity = INVALID_CLASS_ID
+        if 'class_affinity' in pref:
+            class_affinity = pref['class_affinity']
+        num_min_tream_members = 0
+        if 'num_min_tream_members' in pref:
+            num_min_tream_members = pref['num_min_tream_members']
+        preferences[name] = Preference(class_affinity, num_min_tream_members)
+
+    return preferences
 
 def main():
     num_teams = 3
     num_members = 8
     teams = [i for i in range(num_teams)]
     members = [i for i in range(num_members)]
-    history = []
-    history.append([{"taro", "jiro", "saburo"}, {"kenta", "takashi"}, {"takuya", "hayato", "masao"}])
-    history.append([{"taro", "kenta", "takuya"}, {"jiro", "masao"}, {"saburo", "takashi", "hayato"}])
-    history.append([{"taro", "saburo", "hayato"}, {"kenta", "takuya"}, {"jiro", "takashi", "masao"}])
+    history, preferences = load_input(sys.argv[1])
 
     # initial assignment
     init_state = []
-    init_state.append({"taro", "jiro", "saburo"})
-    init_state.append({"kenta", "takashi"})
-    init_state.append({"takuya", "hayato", "masao"})
-
-    preferences = {
-        "taro": Preference(num_min_team_members=3),
-        "jiro": Preference(num_min_team_members=3),
-        "saburo": Preference(),
-        "kenta": Preference(),
-        "takashi": Preference(class_affinity=2),
-        "takuya": Preference(),
-        "hayato": Preference(),
-        "masao": Preference(),
-    }
+    membersIndex = 0
+    for num_members in settings.num_members_in_each_team:
+        tmpTeam = set()
+        for i in range(num_members):
+            tmpTeam.add(settings.members[membersIndex])
+            membersIndex += 1
+        init_state.append(copy.copy(tmpTeam))
+        tmpTeam.clear()
 
     prob = SortingHat(init_state, preferences, history)
     prob.steps = 100000
