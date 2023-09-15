b501b561b02b840f9f3421b8f607c4ba62d41b1d
Shinya Hayashi <shinyaUT0925@gmail.com>
2023-09-15 12:54:36 +0900

    add `diff_team_prefer_rate` parameter
    Signed-off-by: Shinya Hayashi <shinyaUT0925@gmail.com>

--- debug.py
+++ debug.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import sys
 import argparse
 import yaml
 import evaluator
@@ -14,7 +15,15 @@ def main():
                         help='state to be checked in YAML format \
                             (e.g. {"state": [["hayato", "taro", "masao"], ["kenta", "saburo"], \
                                 ["takashi", "jiro", "takuya"]]})')
+    parser.add_argument('--diff_team_prefer_rate', dest='diff_team_prefer_rate', action='store',
+                        type=float, default=1.0,
+                        help='(advanced option) see implementation of evaluator')
     args = parser.parse_args()
+
+    if args.diff_team_prefer_rate < 0:
+        print(
+            f"invalid value of diff_team_prefer_rate: {args.diff_team_prefer_rate}")
+        sys.exit(1)
 
     # parse YAML input
     state_yaml = yaml.safe_load(args.raw_state)
@@ -25,7 +34,8 @@ def main():
     print(f"state: {state}")
     settings, preferences, history = input_handler.load(args.filename)
     ev = evaluator.Evaluator(preferences, history,
-                             settings.num_remaining_members_in_each_team)
+                             settings.num_remaining_members_in_each_team,
+                             args.diff_team_prefer_rate)
 
     v = ev.evaluate(state)
     print(f"value: {v}")
